742301f9c01bc3ec2f6ce43de4863e006578f009
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-23 11:33:36 +0545

    changes occured while moving files to specific folder

--- config/default.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3000,
-  mongoURI: "mongodb+srv://sanjay:12345@cluster0.sa4sbyc.mongodb.net/sample_mflix?retryWrites=true&w=majority"
-}
--- routes/comment-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/comment-controller');
-
-// Route to retrieve all comments related to a specific movie
-router.get('/comments/:movieId', commentController.getCommentsByMovieId);
-
-module.exports = router;
